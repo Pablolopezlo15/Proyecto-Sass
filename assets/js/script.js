38004bce743f06408351c34270c074a5079f8ad2
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2024-02-06 20:52:40 +0100

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let currentSlide = 1;
+
+function changeSlide(n) {
+    showSlide(currentSlide += n);
+}
+
+function showSlide(n) {
+    const slides = document.querySelector('.slides2');
+    if (n > 3) { currentSlide = 1; }
+    if (n < 1) { currentSlide = 3; }
+    slides.style.transform = `translateX(${-(currentSlide - 1) * 100}%)`;
+}
