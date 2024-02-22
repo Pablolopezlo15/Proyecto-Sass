efe7bacf4e6a923d944bb66a827c2772a2c66d36
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2024-02-22 12:03:00 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
 // Importa Gulp y los complementos necesarios
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 
+function compileSass() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('subir/css'));
+}
 
 function minifyCss() {
     return gulp.src('css/*.css')
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('subir/css'));
 }
 
 function minifyHtml() {
     return gulp.src('*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('subir'));
 }
 
 function moveHtml() {
     return gulp.src('*.html')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('subir'));
 }
 
+function moveAssets() {
+    return gulp.src('assets/**/*')
+        .pipe(gulp.dest('subir/assets'));
+}
+
+exports.compileSass = compileSass; 
 exports.minifyCss = minifyCss;
 exports.minifyHtml = minifyHtml;
 exports.moveHtml = moveHtml;
-exports.build = gulp.series(minifyHtml, moveHtml);
+exports.moveAssets = moveAssets;
+exports.build = gulp.series(compileSass, minifyHtml, minifyCss, moveHtml, moveAssets);
